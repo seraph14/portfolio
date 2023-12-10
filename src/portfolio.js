7dd565273e6daaaf3c9f1697fb1ede8a6f3d7810
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2023-12-10 17:44:33 +0000

    a

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://seraph15.github.io/portfolio',
+  homepage: 'https://seraph14.github.io/portfolio',
   title: 'SK.',
 }
 
