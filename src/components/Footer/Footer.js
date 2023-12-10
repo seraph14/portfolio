91b5191588247a0765cc23d4267a5d062360c03f
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2023-12-10 17:41:49 +0000

    a

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/seraph14/portfolio'
       className='link footer__link'
     >
       Surafel Kassahun ©2023. All rights reserved.
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://seraph15.github.io/portfolio',
   title: 'SK.',
 }
 
