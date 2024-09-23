12e16254403a2b22921e40ac982203149e3d222e
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2024-09-23 09:43:05 +0300

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -52,14 +52,7 @@ const experiences = [
   {
     name: 'Software Engineer, A2SV',
     description: `
-• Atrons - Educational Resource Sharing Platform
-  • Led the development of Atrons, a mobile and web platform built using Python and React/Next.js that empowers users with free educational resources. The project was awarded $8,000 by a jury led by Dr. Jelani Nelson and Ricardo Baeza-Yates.
-  
-• AKIL - Connecting NPOs & Volunteers
-  • Contributed to the entire software development lifecycle of an MVP. Developing the mobile app with Flutter and integrating RESTful APIs using Go.
-  
-• Head of Education - Educator
-  • Led interview preparation sessions in Python, delivered clear explanations to resolve doubts, and provided individualized guidance through 1:1 mentorship.
+I led the development of Atrons, an educational platform using Python (backend), Flutter (mobile) and React/Next.js (web), which won an $8,000 award. I also contributed to AKIL, developing its mobile app with Flutter and integrating APIs with Go. As Head of Education, I guided interview prep in Python and provided 1:1 mentorship.
 `,
     stack: [
       'Django',
@@ -76,9 +69,7 @@ const experiences = [
   },
   {
     name: 'Software Engineer, PlayerTrader',
-    description: `
-• PlayerTrader (www.playertrader.net) is a fantasy sports website with data-driven analytics.
-  • Developed, maintained, and enhanced backend and frontend features of the platform.
+    description: `PlayerTrader is a fantasy sports website with data-driven analytics. Developed, maintained, and enhanced backend and frontend features of the platform.
 `,
     stack: [
       'Typescript',
