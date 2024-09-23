72aa81af665cb6012a5c775c68b44333f7f4774c
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2024-09-23 08:26:27 +0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,7 +6,7 @@ const header = {
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'Surafel K',
+  name: 'Surafel A',
   role: 'Software Engineer',
   description:
     "A software engineer with a strong foundation in cross-platform mobile development and E-Commerce solutions. Armed with a Bachelor's degree in Software Engineering and a proven track record in leading successful projects, I bring technical expertise and innovation to every endeavor, fostering positive impact.",
@@ -52,15 +52,15 @@ const experiences = [
   {
     name: 'Software Engineer, A2SV',
     description:
-      'Participated in the development of a mobile app called Hakimhub a medical information and an appointment system for Hospitals in Ethiopia.',
-    stack: ['Django', 'Flutter', 'Dart', 'Bloc'],
+      'Atrons - Educational Resource Sharing Platform Led the development of Atrons, a mobile and web platform built using Python and React/Next.js that empowers users with free educational resources. The project was awarded $8,000 by a jury led by Dr. Jelani Nelson and Ricardo Baeza-Yates. AKIL - Connecting NPOs & Volunteers Contributed to the entire software development lifecycle of an MVP. Built RESTful APIs using GO. Head of Education - Educator Led interview preparation sessions in Python, delivered clear explanations to resolve doubts and provided individualized guidance through 1:1 mentorship.',
+    stack: ['Django', 'Next.js', 'React.js', 'TailwindCSS', 'Flutter', 'Dart', 'Bloc'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Head of Education',
+    name: 'Software Engineer, PlayerTrader',
     description:
-      'Managing project development for Atrons: a platform developed by the group to share educational resources for free.',
+      'PlayerTrader (www.playertrader.net) is a fantasy sports website with data-driven analytics. Developed, maintained, and enhanced backend and frontend features of the platform.',
     stack: ['Flutter', 'Dart', 'Vue.js', 'Leadership', 'DSA'],
     sourceCode: 'https://github.com/',
     livePreview: 'https://github.com/',
@@ -73,6 +73,7 @@ const skills = [
   'Flutter',
   'Android',
   'React',
+  'Next.js',
   'HTML',
   'CSS',
   'Python',
